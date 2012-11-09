a281c46995b30dde5b77bebc81a4481a5f6f229c
aldakur <aldakur@gmail.com>
2012-11-09 17:18:38 +0100

    Lehen igoera

---
+++ 02-Resources/src/net/aldakur/app/unutopia/SplashActivity.java
@@ -0,0 +1,18 @@
+package net.aldakur.app.unutopia;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+
+public class SplashActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        
+        
+        
+    }
+
+}
